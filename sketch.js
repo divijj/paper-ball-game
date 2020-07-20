75656295432665234c062267b533770bfdd1a9be
divijj <64677825+divijj@users.noreply.github.com>
2020-07-20 14:40:17 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -39,3 +39,8 @@ function draw() {
  ball.display();
  ground.display();
 }
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:85,y:-85});
+	}
+}
